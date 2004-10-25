adc0151732e1f75f2417eccaf173571877dc1818
viperal <viperal@538a1c98-dde4-0310-b955-efc6994462f9>
2004-10-25 00:32:39 +0000

    Forgot to commit latest error.php. It checks for the users lang and attempts to get the error file.

--- cms1/error.php
+++ cms1/error.php
@@ -12,6 +12,8 @@
 //**************************************************************//
 define('VIPERAL', 'CMS');
 
+error_reporting(0);
+
 if (extension_loaded('zlib'))
 {
 	ob_start('ob_gzhandler');
@@ -19,22 +21,38 @@ if (extension_loaded('zlib'))
 
 //$site_file_root = getenv('DOCUMENT_ROOT').'/';
 $site_file_root = 'C:/Program Files/Apache Group/Apache2/cms/';
+$lang = 'en';
 
-if (isset($_GET['error']) && is_numeric($_GET['error']))
+if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE']))
 {
-	error_reporting(0);
-	
-	require($site_file_root.'language/error.php');
-	require($site_file_root.'includes/smarty/Smarty.class.php');
-	
-	$_CLASS['template'] =& new Smarty();
-	
-	$_CLASS['template']->assign('MESSAGE_TEXT',  (empty($error[$_GET['error']]) ? $error['404'] : $error[$_GET['error']]));
-			
-	$_CLASS['template']->display('error.html');
-	
+	$accept_lang_array = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+	foreach ($accept_lang_array as $accept_lang)
+	{
+		$accept_lang = substr($accept_lang, 0, 2) . '_' . strtoupper(substr($accept_lang, 3, 2));
+		if (file_exists($site_file_root.'language/' . $accept_lang . '/error.php'))
+		{
+			$lang = $accept_lang;
+			break;
+		}
+		
+		$accept_lang = substr($accept_lang, 0, 2);
+		if (file_exists($site_file_root.'language/' . $accept_lang . '/error.php'))
+		{
+			$lang = $accept_lang;
+			break;
+		}
+	}
 }
 
+require($site_file_root.'language/' . $lang . '/error.php');
+require($site_file_root.'includes/smarty/Smarty.class.php');
+
+$_CLASS['template'] =& new Smarty();
+
+$_CLASS['template']->assign('MESSAGE_TEXT',  (empty($error[$_GET['error']]) ? $error['404'] : $error[$_GET['error']]));
+		
+$_CLASS['template']->display('error.html');
+	
 die;
 
 ?>
