64ac2a2d033e5c8d7fcddd9960aa622e60b657de
viperal <viperal@538a1c98-dde4-0310-b955-efc6994462f9>
2004-10-29 07:04:58 +0000

    Small updates and fixes for whats there. Removed gallery v1 - It wouldn't be there in offical.

--- BETA/modules.php
+++
@@ -1,4 +0,0 @@
-<?php
-// blaaaaa
-include('index.php');
-?>
---
+++ cms1/blocks/block-theme_select.php
@@ -0,0 +1,31 @@
+<?php
+//**************************************************************//
+//  Vipeal CMS:													//
+//**************************************************************//
+//																//
+//  Copyright © 2004 by Viperal									//
+//  http://www.viperal.com										//
+//																//
+//  Viperal CMS is released under the terms and conditions		//
+//  of the GNU General Public License version 2					//
+//																//
+//**************************************************************//
+
+if (!defined('VIPERAL'))
+{
+    Header('Location: ../');
+    die();
+}
+
+$this->content = '
+<div style="text-align: center;">
+	<form action="'.$_SERVER['REQUEST_URI'].'" method="post">
+		<select name="prevtheme" onchange="submit();">
+'.theme_select().'
+		</select>
+		<br /><br /><input class="btnlite" name="" value="Select" type="submit" />
+	</form>
+</div>
+';
+
+?>
