fd5c710734e3229ccb1f3602fde4040870a30363
viperal <viperal@538a1c98-dde4-0310-b955-efc6994462f9>
2005-05-30 04:17:49 +0000

    Now add the Quick_Message changes

--- cms1/modules/Quick_Message/index.php
+++ cms1/modules/Quick_Message/index.php
@@ -16,57 +16,74 @@ if (!defined('VIPERAL'))
     header('location: ../../');
     die();
 }
+switch (get_variable('mode', 'GET', false))
+{
+	case 'add':
+		add_message();
+		break;
+		
+	case 'all':
+		show_messages(true);
+		
+	case 'delete':
+		delete_message();
+	
+	default:
+		show_messages();
+		break;
+}
 
-function add_shout()
+function add_message()
 {
 	if (empty($_POST['submit']))
 	{
-		return show_shouts();
+		return show_messages();
 	}
 	
 	global $_CLASS, $prefix, $_CORE_CONFIG;
 		
 	$_CLASS['core_user']->add_lang();
-	$_CORE_CONFIG['Shoutblock']['lastpost_check'] = 300;
+	$_CORE_CONFIG['quick_message']['lastpost_check'] = 300;
 
-	$user_id = (is_user()) ? $_CLASS['core_user']->data['user_id'] : '';
+	$user_id = $_CLASS['core_user']->data['user_id'];
 	$user_name = (is_user()) ? $_CLASS['core_user']->data['username'] : get_username();
 	
-    if(!$shout = get_variable('shout', 'POST', false))
+    if(!$message = get_variable('message', 'POST', false))
     {
 		trigger_error($_CLASS['core_user']->lang['NO_MESSAGE']); 
     }
     
-    $length = strlen($shout);
+    $length = strlen($message);
     
     if($length < 2)
     {
 		trigger_error($_CLASS['core_user']->lang['SHORT_MESSAGE']); 
     }
     
-    if($length > $_CORE_CONFIG['Shoutblock']['maxlength']) // this shouldn't happen but still check
+    if($length > $_CORE_CONFIG['quick_message']['maxlength'])
     { 
 		trigger_error($_CLASS['core_user']->lang['LONG_MESSAGE']);
     }
    
-	$shout 		= htmlentities($shout, ENT_QUOTES, 'UTF-8');
+	$message 	= htmlentities($message, ENT_QUOTES, 'UTF-8');
 	$user_name 	= htmlentities($user_name, ENT_QUOTES, 'UTF-8');
    
-    $result = $_CLASS['core_db']->sql_query('SELECT COUNT(*) as count FROM '.$prefix."_shoutblock WHERE shout='".$_CLASS['core_db']->sql_escape($shout)."' AND time >= '".(time() - $_CORE_CONFIG['Shoutblock']['lastpost_check'])."' LIMIT 1");
+    $result = $_CLASS['core_db']->sql_query('SELECT COUNT(*) as count FROM '.$prefix."_quick_message WHERE message='".$_CLASS['core_db']->sql_escape($message)."' AND time >= '".(time() - $_CORE_CONFIG['quick_message']['lastpost_check'])."' LIMIT 1");
 	$count = $_CLASS['core_db']->sql_fetchrow($result);
     $_CLASS['core_db']->sql_freeresult($result);
 
-    if ($count['count'] > 0) // add a count check here so it admin ajustable
+// add a count check here so it admin ajustable
+    if ($count['count'] > 0)
     {
-        trigger_error(sprintf($_CLASS['core_user']->lang['SAME_MESSAGE'], $_CORE_CONFIG['Shoutblock']['lastpost_check'] / 60));
+        trigger_error(sprintf($_CLASS['core_user']->lang['SAME_MESSAGE'], $_CORE_CONFIG['quick_message']['lastpost_check'] / 60));
     }
     
     $_CLASS['core_db']->sql_freeresult($result);
 
-	$sql = 'INSERT INTO '.$prefix.'_shoutblock ' . $_CLASS['core_db']->sql_build_array('INSERT', array(
+	$sql = 'INSERT INTO '.$prefix.'_quick_message ' . $_CLASS['core_db']->sql_build_array('INSERT', array(
 		'user_name'	=> $user_name,
 		'user_id'	=> $user_id ,
-		'shout'		=> $shout,
+		'message'	=> $message,
 		'time'		=> time(),
 		'ip'		=> $_CLASS['core_user']->ip));
 		
@@ -77,27 +94,27 @@ function add_shout()
 }
 
     
-function show_shouts($all = false)
+function show_messages($all = false)
 {
     global $prefix, $_CLASS, $config, $_CORE_CONFIG;
 	
 	$_CLASS['core_user']->add_lang();
 	$_CLASS['core_user']->add_img(false, 'Forums');
 
-	$_CORE_CONFIG['Shoutblock']['simplemode'] = true;
+	$_CORE_CONFIG['quick_message']['simplemode'] = true;
 	
 	$start = ($all) ? '' : get_variable('start', 'GET', '', 'integer');
 	
-	$limit = ($all) ? '' : $_CORE_CONFIG['Shoutblock']['number'];
+	$limit = ($all) ? '' : $_CORE_CONFIG['quick_message']['number'];
 	
-	if ($_CORE_CONFIG['Shoutblock']['simplemode'])
+	if ($_CORE_CONFIG['quick_message']['simplemode'])
 	{
-		$sql = 'SELECT s.*, u.user_karma FROM '.$prefix.'_shoutblock s LEFT JOIN ' . USERS_TABLE . ' u  ON (u.user_id = s.user_id) ORDER BY time DESC';
+		$sql = 'SELECT s.*, u.user_karma FROM '.$prefix.'_quick_message s LEFT JOIN ' . USERS_TABLE . ' u  ON (u.user_id = s.user_id) ORDER BY time DESC';
 
 	} else {
 
 		$sql = 'SELECT u.user_colour, u.user_karma,u.user_regdate, u.user_allow_viewonline, u.user_rank, u.user_sig, u.user_sig_bbcode_uid, u.user_sig_bbcode_bitfield, u.user_avatar, u.user_avatar_type, u.user_avatar_width, u.user_avatar_height, s.*
-			FROM '.$prefix.'_shoutblock s 
+			FROM '.$prefix.'_quick_message s 
 			LEFT JOIN ' . USERS_TABLE . ' u  ON (u.user_id = s.user_id)
 			order by time DESC';
 	}
@@ -110,6 +127,7 @@ function show_shouts($all = false)
 		'L_POSTER'			=> $_CLASS['core_user']->lang['POSTER'],
 		'L_POSTED'			=> $_CLASS['core_user']->lang['POSTED'],
 		'L_MESSAGE'			=> $_CLASS['core_user']->lang['MESSAGE'],
+		'L_DELETE'			=> $_CLASS['core_user']->get_lang('DELETE'),
 		'ERROR'				=> $error, 
 		)
 	);
@@ -118,9 +136,9 @@ function show_shouts($all = false)
 	{
 		$_CLASS['core_display']->display_head();
 		 
-		if ($_CORE_CONFIG['Shoutblock']['simplemode'])
+		if ($_CORE_CONFIG['quick_message']['simplemode'])
 		{
-			$_CLASS['core_template']->display('modules/Shoutbox/index.html');	
+			$_CLASS['core_template']->display('modules/Quick_Message/index.html');	
 		}
 		
 		$_CLASS['core_display']->display_footer();
@@ -139,9 +157,10 @@ function show_shouts($all = false)
 			$userlink = false;
 		}
 
-		if (!$_CORE_CONFIG['Shoutblock']['simplemode'] && $row['user_avatar'] && $_CLASS['core_user']->optionget('viewavatars'))
+		if (!$_CORE_CONFIG['quick_message']['simplemode'] && $row['user_avatar'] && $_CLASS['core_user']->optionget('viewavatars'))
 		{
 			$avatar_img = '';
+			
 			switch ($row['user_avatar_type'])
 			{
 				case AVATAR_UPLOAD:
@@ -151,22 +170,27 @@ function show_shouts($all = false)
 					$avatar_img = $config['avatar_gallery_path'] . '/';
 					break;
 			}
+			
 			$avatar_img .= $row['user_avatar'];
 			$avatar = '<img src="' . $avatar_img . '" width="' . $row['user_avatar_width'] . '" height="' . $row['user_avatar_height'] . '" border="0" alt="" />';
-		} else {
+			
+		}
+		else
+		{
 			$avatar = false;
 		}
 
 		if ($row['user_id'])
 		{
-			$row['shout'] = preg_replace('#\[url=([^\[]+?)\](.*?)\[/url\]#s', '<a href="$1" target="_blank">$2</a>', $row['shout']);
+			$row['message'] = preg_replace('#\[url=([^\[]+?)\](.*?)\[/url\]#s', '<a href="$1" target="_blank">$2</a>', $row['message']);
 		}
 		
-		$_CLASS['core_template']->assign_vars_array('shout', array(
+		$_CLASS['core_template']->assign_vars_array('quick_message', array(
 			'USER_NAME'		=> $user_name,
 			'USER_LINK'		=> $userlink,
-			'SHOUT'			=> trim_text($row['shout'], '<br />'),
-			'TIME'			=> ($_CORE_CONFIG['Shoutblock']['time']) ? $_CLASS['core_user']->format_date($row['time']) : '',
+			'DELETE_LINK'	=> ($row['time'] > ($_CLASS['core_user']->time - 18000)) ? generate_link('quick_message&amp;mode=delete&amp;id='.$row['id']) : '',
+			'MESSAGE'		=> trim_text($row['message'], '<br />'),
+			'TIME'			=> $_CLASS['core_user']->format_date($row['time']),
 			'KARMA_IMG'		=> ($row['user_id'] && $config['enable_karma']) ? $_CLASS['core_user']->img('karma_center', $_CLASS['core_user']->lang['KARMA'][$row['user_karma']], false, (int) $row['user_karma']) : '',
 			'POSTER_AVATAR' => $avatar,
 			//'ONLINE_IMG'	=> ($row['user_id']) ? $_CLASS['core_user']->img('btn_online', 'ONLINE') : '', 
@@ -176,43 +200,83 @@ function show_shouts($all = false)
 	}
 	while ($row = $_CLASS['core_db']->sql_fetchrow($result));
 
-
-	$result = $_CLASS['core_db']->sql_query('SELECT COUNT(*) AS total from '.$prefix.'_shoutblock');
+	$result = $_CLASS['core_db']->sql_query('SELECT COUNT(*) AS total from '.$prefix.'_quick_message');
 	$row = $_CLASS['core_db']->sql_fetchrow($result);
 	$_CLASS['core_db']->sql_freeresult($result);	
 
 	$_CLASS['core_user']->img['pagination_sep'] = ' | ';
 	
-	$pagination = generate_pagination('Shoutblock', $row['total'], $limit, $start, false, 'SHOUT_');
-	$_CLASS['core_template']->assign('SHOUT_PAGINATION', $pagination);
-	
+	$_CLASS['core_template']->assign(array(
+		'Q_MESSAGE_PAGINATION'	=> generate_pagination('quick_message', $row['total'], $limit, $start, false, 'Q_MESSAGE_'),
+		'Q_PAGE_NUMBER'			=> on_page($row['total'], $limit, $start),
+		'Q_TOTAL_MESSAGES'		=> $row['total']
+	));
 	
 	$_CLASS['core_display']->display_head();
 	 
-	if ($_CORE_CONFIG['Shoutblock']['simplemode'])
+	if ($_CORE_CONFIG['quick_message']['simplemode'])
 	{
-		$_CLASS['core_template']->display('modules/Shoutbox/index.html');	
+		$_CLASS['core_template']->display('modules/Quick_Message/index.html');	
 	}
 	
 	$_CLASS['core_display']->display_footer();
 }
 
+function delete_message()
+{
+
+	global $_CORE_CONFIG, $_CLASS, $prefix;
+	
+	$id = (int) get_variable('id', 'GET');
+	
+	$result = $_CLASS['core_db']->sql_query('SELECT * FROM '.$prefix.'_quick_message WHERE id='.$id);
+	$row = $_CLASS['core_db']->sql_fetchrow($result);
+	$_CLASS['core_db']->sql_freeresult($result);
+	
+	if (!$row)
+	{
+		trigger_error('MESSAGE_NOT_FOUND');
+	}
+	
+	if ($row['time'] < ($_CLASS['core_user']->time - 18000))
+	{
+	
+	}
+	
+	if ($row['id'] != ANONYMOUS)
+	{
+		if ($row['user_id'] != $_CLASS['core_user']->data['user_id'])
+		{
+			trigger_error('NOT_USER_MESSAGE');
+		}
+	}
+	elseif ($_CLASS['core_user']->ip != $row['ip'])
+	{
+		trigger_error('NOT_USER_MESSAGE');
+	}
+	
+	$sql = 'DELETE FROM '.$prefix.'_quick_message WHERE id='.$id;
+	$_CLASS['core_db']->sql_query($sql);
+	
+	trigger_error('MESSAGE_DELETED');
+}
+
 function get_username()
 {
 
-	global $_CORE_CONFIG, $_CLASS, $site_file_root, $phpEx;
+	global $_CORE_CONFIG, $site_file_root;
 	
 	$user_name = trim(get_variable('user_name', 'POST', ''));
 
 	if (!$user_name)
 	{
-		if ($_CORE_CONFIG['Shoutblock']['allow_anonymous'] == '2')
+		if ($_CORE_CONFIG['quick_message']['allow_anonymous'] == '2')
 		{
 			return false;
 			
 		} else {
 		
-			trigger_error($_CLASS['core_user']->lang['NO_NAME']);
+			trigger_error('NO_NAME');
 		}
 	}
 	
@@ -220,35 +284,21 @@ function get_username()
 	
 	if ($length < 2)
 	{
-		trigger_error($_CLASS['core_user']->lang['SHORT_NAME']);
+		trigger_error('SHORT_NAME');
 	}
 	
 	if ($length > 10)
 	{
-		trigger_error($_CLASS['core_user']->lang['LONG_NAME']);
+		trigger_error('LONG_NAME');
 	}
 	
-	require($site_file_root.'includes/forums/functions_user.' . $phpEx);
+	require($site_file_root.'includes/forums/functions_user.php');
 	if ($error = validate_username($user_name))
 	{
 		trigger_error($error);
 	}
 	return $user_name;
 
-}
-
-switch (get_variable('mode', 'GET', ''))
-{
-	case 'shout':
-		add_shout();
-		break;
-		
-	case 'all':
-		show_shouts(true);
-	
-	default:
-		show_shouts();
-		break;
 }
 
 ?>
